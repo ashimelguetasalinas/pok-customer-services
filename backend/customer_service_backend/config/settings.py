47cf0bbf2c1e663765a355c0235a871337dc0805
Ashim Elgueta <aelgueta@planok.com>
2026-01-16 13:18:03 -0300

    Correcciones

--- backend/customer_service_backend/config/settings.py
+++ backend/customer_service_backend/config/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "customer_service",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
--- backend/customer_service_backend/entrypoint.sh
+++ backend/customer_service_backend/entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+
 set -e
 
-echo "🟢 Aplicando migraciones..."
+echo "⏳ Esperando base de datos..."
+
+# Espera a la DB (PostgreSQL)
+if [ "$DB_ENGINE" = "postgres" ]; then
+  while ! nc -z "$DB_HOST" "$DB_PORT"; do
+    sleep 1
+  done
+fi
+
+echo "✅ Base de datos disponible"
+
+echo "📦 Ejecutando migraciones..."
 python manage.py migrate --noinput
 
-echo "🟢 Iniciando servidor Django..."
+echo "📁 Recolectando estáticos (si aplica)..."
+python manage.py collectstatic --noinput || true
+
+echo "🚀 Iniciando aplicación..."
 exec "$@"
